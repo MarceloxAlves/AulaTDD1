33a7e7ec11c0e580dcd3242898e9d253e971ea9c
Marcelo <marceloalvessoft@gmail.com>
2018-07-13 15:31:58 -0300

    aparente implementado

--- src/test/java/TestFracao.java
+++ src/test/java/TestFracao.java
@@ -8,44 +8,50 @@ public class TestFracao {
     Fracao fracao;
 
     @Before
-    public void setup(){
-        fracao = new Fracao(1,2);
+    public void setup() {
+        fracao = new Fracao(1, 2);
     }
 
     @Test
-    public void test_iniciar_fracao(){
+    public void test_iniciar_fracao() {
         assertEquals("1/2", fracao.toString());
     }
 
     @Test
-    public void test_valor_real(){
+    public void test_valor_real() {
         assertEquals(0.5, fracao.valorReal(), 0.1);
     }
 
 
     @Test
-    public void test_valor_soma(){
-        assertEquals("5/6", fracao.soma(new Fracao(1,2), new Fracao(1,3)).toString());
+    public void test_valor_soma() {
+        assertEquals("5/6", fracao.soma(new Fracao(1, 2), new Fracao(1, 3)).toString());
     }
 
     @Test
-    public void test_valor_diff(){
+    public void test_valor_diff() {
 
-        assertEquals("1/6", fracao.diff(new Fracao(1,2), new Fracao(1,3)).toString());
-    }
-
-
-    @Test
-    public void test_valor_div(){
-
-        assertEquals("3/2", fracao.div(new Fracao(1,2), new Fracao(1,3)).toString());
+        assertEquals("1/6", fracao.diff(new Fracao(1, 2), new Fracao(1, 3)).toString());
     }
 
 
     @Test
-    public void test_valor_mult(){
+    public void test_valor_div() {
 
-        assertEquals("1/6", fracao.mult(new Fracao(1,2), new Fracao(1,3)).toString());
+        assertEquals("3/2", fracao.div(new Fracao(1, 2), new Fracao(1, 3)).toString());
+    }
+
+
+    @Test
+    public void test_valor_mult() {
+
+        assertEquals("1/6", fracao.mult(new Fracao(1, 2), new Fracao(1, 3)).toString());
+    }
+
+    @Test
+    public void test_valor_aparente_falso() {
+
+        assertEquals(false, fracao.aparente());
     }
 
 }
